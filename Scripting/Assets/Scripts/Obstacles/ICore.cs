007d5269bd9ace33831d92e72a3b7e3983554ee7
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-03-27 13:40:55 -0500

    Obstacles advance (Necesary .gitignore update)

---
+++ Scripting/Assets/Scripts/Obstacles/ICore.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesVacias
+{
+    interface ICore
+    {
+        public void cuandoMuere()
+        {
+
+        }
+
+        public void cuandoGana()
+        {
+
+        }
+    }
+}
