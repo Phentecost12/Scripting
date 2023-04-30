5f9d2a9c7ef3505a72e48dd75db9adac2a7d349c
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-04-30 16:05:59 -0500

    Camera Movement Limits
    Another Necessary .gitignore update

--- Scripting/Assets/cameraBehavior.cs
+++ Scripting/Assets/cameraBehavior.cs
@@ -9,11 +9,37 @@ public class cameraBehavior : MonoBehaviour
     [SerializeField]private float smoothTime = 5f;
     private Vector3 velocity = Vector3.zero;
 
+    [Header("Camera Properties")]
     [SerializeField] private Transform target;
+    [SerializeField] private DungeonManager dungeonManager;
+    public  BoxCollider2D boundary;
+
+    private float minX, maxX, minY, maxY;
+
+    void Start()
+    {
+        minX = boundary.bounds.min.x; maxX = boundary.bounds.max.x;
+        minY = boundary.bounds.min.y; maxY = boundary.bounds.max.y;
+    }
 
     void Update()
     {
         ChangePos();
+    }
+
+    private void LateUpdate()
+    {
+        // Obtener la posición actual de la cámara
+        Vector3 posicionCamara = transform.position;
+
+        // Limitar el movimiento en el eje X
+        posicionCamara.x = Mathf.Clamp(posicionCamara.x, minX, maxX);
+
+        // Limitar el movimiento en el eje Y
+        posicionCamara.y = Mathf.Clamp(posicionCamara.y, minY, maxY);
+
+        // Asignar la nueva posición a la cámara
+        transform.position = posicionCamara;
     }
 
     public void ChangePos()
@@ -21,4 +47,14 @@ public class cameraBehavior : MonoBehaviour
         Vector3 targetPos = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
     } 
+
+    /*
+    private void SetBoundary(Vector2 boundarySize)
+    {   
+        BoxCollider2D boundary = gameObject.AddComponent<BoxCollider2D>();
+
+        boundary.size = boundarySize;
+        boundary.offset = Vector2.zero;
+    }
+    */
 }
