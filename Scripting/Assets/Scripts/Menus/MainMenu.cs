7c6e56d1c30f0b8ae8eb35e219c5e829651feff2
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-04-30 20:59:26 -0500

    Menus Added

---
+++ Scripting/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SettingsBtn()
+    {
+
+    }
+
+    public void QuitBtn()
+    {
+        Debug.Log("Exiting game...");
+        Application.Quit();
+    }
+}
